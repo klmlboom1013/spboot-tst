f47ed5141e49b4b39f7f2a077b236522c05777e4
klmlboom1013 <klmlboom1013@gmail.com>
2018-11-25 21:35:00 +0900

    Controler 추가
    1. WelcomeController.java
    2. pom.xml -> "thymeleaf" dependency 추가

---
+++ src/main/java/com/lhs/web/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.lhs.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+	@GetMapping("/welcome")
+	public String welcome(Model model) {
+		model.addAttribute("result","Hello, World!!");
+		return "welcome";
+	}
+	
+}
