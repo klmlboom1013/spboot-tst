5d50313fbe26db2e4cd6ae6e95875fe6055f2358
klmlboom1013 <klmlboom1013@gmail.com>
2018-12-01 19:08:27 +0900

    URL 리펙토링
    * controller url 변경
    * templete 내 중복 태그 include 처리

--- src/main/java/com/lhs/web/controller/UserController.java
+++ src/main/java/com/lhs/web/controller/UserController.java
@@ -5,28 +5,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.lhs.domain.User;
 import com.lhs.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
 	
-	@PostMapping("/create")
+	@PostMapping("")
 	public String create(Model model, User data) {
 		model.addAttribute("userId",data.getUserId());
 		model.addAttribute("name",data.getName());
 		model.addAttribute("email",data.getEmail());
 		userRepository.save(data);
-		return "redirect:/list";
+		return "redirect:/users";
 	}
 	
-	@GetMapping("/list")
+	@GetMapping("")
 	public String list(Model model, User data) {
 		model.addAttribute("users", userRepository.findAll());
-		return "list";
+		return "/user/list";
 	}
 }
