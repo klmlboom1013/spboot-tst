734c89eea0337cb6e86c98f31f1467b0da788430
klmlboom1013 <klmlboom1013@gmail.com>
2018-12-02 20:33:11 +0900

    소스정리
    * static 경로 html 파일 -> template 경로로 이동

--- src/main/java/com/lhs/web/controller/UserController.java
+++ src/main/java/com/lhs/web/controller/UserController.java
@@ -15,43 +15,47 @@ import com.lhs.domain.UserRepository;
 @Controller
 @RequestMapping("/users")
 public class UserController {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	
+
 	@GetMapping("/form")
 	public String form() {
 		return "/user/form";
 	}
-	
+
 	@PostMapping("")
 	public String create(Model model, User data) {
-		model.addAttribute("userId",data.getUserId());
-		model.addAttribute("name",data.getName());
-		model.addAttribute("email",data.getEmail());
+		model.addAttribute("userId", data.getUserId());
+		model.addAttribute("name", data.getName());
+		model.addAttribute("email", data.getEmail());
 		userRepository.save(data);
 		return "redirect:/users";
 	}
-	
+
 	@GetMapping("")
 	public String list(Model model, User data) {
 		model.addAttribute("users", userRepository.findAll());
 		return "/user/list";
 	}
-	
+
 	@GetMapping("/{id}/form")
 	public String updateForm(Model model, @PathVariable Long id) {
 		User user = this.userRepository.getOne(id);
 		model.addAttribute("user", user);
 		return "/user/updateForm";
 	}
-	
+
 	@PutMapping("/{id}")
 	public String update(Model model, @PathVariable Long id, User modifyUser) {
 		User user = this.userRepository.getOne(id);
 		user.update(modifyUser);
 		this.userRepository.save(user);
 		return "redirect:/users";
+	}
+
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "/user/login";
 	}
 }
