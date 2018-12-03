05e95646a2f139ac36f4da789f6bde6629fd1a7f
klmlboom1013 <klmlboom1013@gmail.com>
2018-12-03 22:55:45 +0900

    로그인 사용자 세션 관리

--- src/main/java/com/lhs/web/controller/UserController.java
+++ src/main/java/com/lhs/web/controller/UserController.java
@@ -22,17 +22,32 @@ import com.lhs.domain.UserRepository;
 @RequestMapping("/users")
 public class UserController {
 
+	private static final String _SESSION_USER_KEY_NM = "sessUser";
+	
+	
 	@Autowired
 	private UserRepository userRepository;
 	
 	@Autowired
 	private HttpSession session;
 
+	
+	
+	/**
+	 * 회원가입 form
+	 * @return
+	 */
 	@GetMapping("/form")
 	public String form() {
 		return "/user/form";
 	}
 
+	/**
+	 * 회원가입 (사용자 생성)
+	 * @param model
+	 * @param data
+	 * @return
+	 */
 	@PostMapping("")
 	public String create(Model model, User data) {
 		model.addAttribute("userId", data.getUserId());
@@ -42,32 +57,94 @@ public class UserController {
 		return "redirect:/users";
 	}
 
+	/**
+	 * 회원정보 리스트 조회
+	 * @param model
+	 * @param data
+	 * @return
+	 */
 	@GetMapping("")
 	public String list(Model model, User data) {
+		User sessUser = (User)this.session.getAttribute(_SESSION_USER_KEY_NM);
+		if(ObjectUtils.isEmpty(sessUser)) {
+			return "redirect:/users/loginForm";
+		}
 		model.addAttribute("users", userRepository.findAll());
 		return "/user/list";
 	}
 
+	/**
+	 * 회원 정보 변경 form
+	 * @param model
+	 * @param id
+	 * @return
+	 */
 	@GetMapping("/{id}/form")
 	public String updateForm(Model model, @PathVariable Long id) {
-		User user = this.userRepository.getOne(id);
+		User sessUser = (User)this.session.getAttribute(_SESSION_USER_KEY_NM);
+		if(ObjectUtils.isEmpty(sessUser)) {
+			return "redirect:/users/loginForm";
+		}
+		
+		if(!StringUtils.equals(id, sessUser.getId())) {
+			throw new IllegalStateException("Miss match ID ERROR.");
+		}
+		
+		User user = this.userRepository.getOne(sessUser.getId());
 		model.addAttribute("user", user);
+		
+		session.removeAttribute(_SESSION_USER_KEY_NM);
+		session.setAttribute(_SESSION_USER_KEY_NM, user);
+		
+		
 		return "/user/updateForm";
 	}
 
+	/**
+	 * 회원정보 변경. (업데이트)
+	 * @param model
+	 * @param id
+	 * @param modifyUser
+	 * @return
+	 */
 	@PutMapping("/{id}")
 	public String update(Model model, @PathVariable Long id, User modifyUser) {
+		User sessUser = (User)this.session.getAttribute(_SESSION_USER_KEY_NM);
+		if(ObjectUtils.isEmpty(sessUser)) {
+			return "redirect:/users/loginForm";
+		}
+		
+		if(!StringUtils.equals(id, sessUser.getId())) {
+			throw new IllegalStateException("Miss match ID ERROR.");
+		}
+		
 		User user = this.userRepository.getOne(id);
 		user.update(modifyUser);
 		this.userRepository.save(user);
+		
+		session.removeAttribute(_SESSION_USER_KEY_NM);
+		session.setAttribute(_SESSION_USER_KEY_NM, user);
+		
 		return "redirect:/users";
 	}
 
+	
+	/**
+	 * 로그인 from
+	 * @return
+	 */
 	@GetMapping("/loginForm")
 	public String loginForm() {
 		return "/user/login";
 	}
 
+	
+	/**
+	 * 로그인 실행 (사용자정보 세션 저장)
+	 * @param userId
+	 * @param password
+	 * @return
+	 */
 	@PostMapping("/login")
 	public String login(String userId, String password) {
 		List<User> users = this.userRepository.findByUserId(userId);
@@ -88,14 +165,18 @@ public class UserController {
 		}
 		
 		System.out.println("Login Success [id:"+user.getUserId()+"]");
-		session.setAttribute("sessUser", user);
+		session.setAttribute(_SESSION_USER_KEY_NM, user);
 		
 		return "/user/profile";
 	}
 	
+	/**
+	 * 로그아웃 (세션 시용자 정보 삭제)
+	 * @return
+	 */
 	@GetMapping("/logout")
 	public String logout() {
-		session.removeAttribute("sessUser");
+		session.removeAttribute(_SESSION_USER_KEY_NM);
 		return "redirect:/";
 	}
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,2 @@
 INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES (1, 'klmlboom','1234','이현승','klmlboom@naver.com');
+INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES (2, 'klml1013','1234','이시언','google@naver.com');
