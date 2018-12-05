9d293e9f32b4b14d3aa1767ae19cda0a24baff1f
klmlboom1013 <klmlboom1013@gmail.com>
2018-12-05 22:38:02 +0900

    문의리스트 출력

--- src/main/java/com/lhs/web/controller/HomeController.java
+++ src/main/java/com/lhs/web/controller/HomeController.java
@@ -1,13 +1,25 @@
 package com.lhs.web.controller;
 
+import java.util.List;
+
+import com.lhs.domain.Question;
+import com.lhs.domain.QuestionRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	private QuestionRepository questionRepository;
+
 	@GetMapping("")
-	public String home() {
+	public String home(Model model) {
+		List<Question> questionList = this.questionRepository.findAll();
+		model.addAttribute("questions", questionList);
 		return "index";
 	}
 }
