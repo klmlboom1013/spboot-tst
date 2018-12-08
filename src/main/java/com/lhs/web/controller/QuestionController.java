7db3965a6f0712006f222b43bef26596cc324b9d
klmlboom1013 <klmlboom1013@gmail.com>
2018-12-08 11:25:31 +0900

    질문사항 변경, 삭제 구현

--- src/main/java/com/lhs/web/controller/QuestionController.java
+++ src/main/java/com/lhs/web/controller/QuestionController.java
@@ -1,6 +1,7 @@
 package com.lhs.web.controller;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 
 import javax.servlet.http.HttpSession;
 
@@ -12,10 +13,14 @@ import com.lhs.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -25,8 +30,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/qna")
 public class QuestionController {
 
-    private static final String ROOT_VIEW_PATH = "/qna";
-
     @Autowired
     private HttpSession session;
 
@@ -34,48 +37,74 @@ public class QuestionController {
     private QuestionRepository questionRepository;
 
 
-
     @GetMapping("/form")
     public String form (Model model) {
         if(!HttpSessionUtils.isLoginUser(session))
 			return "redirect:/users/loginForm";
-
-        return goview ("form");
+        return goview ("/form");
     }
 
-    @PostMapping("/regAction")
-    public String regAction (Model model, String title, String contents) {
+    @PostMapping("/form")
+    public String form (Model model, @ModelAttribute Question createQuestion) {
         if(!HttpSessionUtils.isLoginUser(session))
             return "redirect:/users/loginForm";
-        
-        if(StringUtils.isEmpty(title))
+        if(StringUtils.isEmpty(createQuestion.getTitle()))
             return "redirect:/qna/form?errorCode=9990";
-        if(StringUtils.isEmpty(contents))
+        if(StringUtils.isEmpty(createQuestion.getContents()))
             return "redirect:/qna/form?errorCode=9991";
-
         final User sessionUser = HttpSessionUtils.getUserFormSession(session);
-
-        Question question = new Question();
-        question.setWriter(sessionUser);
-        question.setTitle(title);
-        question.setContents(contents);
-        question.setCreateDate(LocalDateTime.now());
-
-        questionRepository.save(question);
+        
+        createQuestion.setWriter(sessionUser);
+        createQuestion.setCreateDate(LocalDateTime.now());
+        questionRepository.save(createQuestion);
 
         return "redirect:/";
     }
 
+    @GetMapping("/form/{id}")
+    public String updateForm(Model model, @PathVariable Long id) {
+        model.addAttribute("question", this.questionRepository.getOne(id));
+        return goview("/updateForm");
+    }
+
+    @PutMapping("/{id}")
+    public String update (Model model, @PathVariable Long id, @ModelAttribute Question updatQuestion){
+        Question question = questionRepository.getOne(id);
+        if(ObjectUtils.isEmpty(question)) {
+            return "redirect:/";
+        }
+        
+        question.setTitle(updatQuestion.getTitle());
+        question.setContents(updatQuestion.getContents());
+
+        this.questionRepository.save(question);
+
+        return "redirect:/qna/show/"+id;
+
+    }
+
     @GetMapping("/show/{id}")
     public String show(Model model, @PathVariable Long id) {
-        Question question = this.questionRepository.getOne(id);
-        model.addAttribute("question", question);
+        model.addAttribute("question", this.questionRepository.getOne(id));
         return goview("/show");
     }
 
+    @DeleteMapping("/{id}")
+    public String delete(Model model, @PathVariable Long id) {
+        Question question = questionRepository.getOne(id);
+        this.questionRepository.delete(question);
+        return "redirect:/";
+    }
 
+
+
+    /**
+     * Web View Path
+     * @param path
+     * @return
+     */
     private String goview (String path) {
-        return ROOT_VIEW_PATH +"/"+ path;
+        return "/qna"+path;
     }
 
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
 INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES (1, 'klmlboom','1234','이현승','klmlboom@naver.com');
 INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES (2, 'klml1013','1234','이시언','google@naver.com');
+
+
+INSERT INTO QUESTION (ID, WRITER_ID, TITLE, CONTENTS) VALUES (1, 1, '테스트질문입니다.','별내용없는 테스트 내용 입니다.');
